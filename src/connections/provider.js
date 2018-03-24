6203b20c50e87ae22803a8be257bee7db5692a0f
Lider equipo rojo <yordi.mosquera@MacBook-Pro-de-Admin-28.local>
2018-03-24 13:24:05 -0500

    Se da inicio a la conexion con firebase

---
+++ src/connections/provider.js
@@ -0,0 +1,54 @@
+import * as firebase from 'firebase';
+
+const activitiesPath = '/places/'
+const participantsPath = '/comments/'
+
+
+class Provider{
+
+
+    static activitiesData() {
+        return new Promise((resolve, reject) => {
+            firebase.database().ref(activitiesPath).once('value', snapshot => {
+                let arrayActivities = [];
+                if (snapshot.val()) {
+                    arrayActivities = this.buildArray(snapshot);
+                }
+                resolve(arrayActivities);
+            }, err => {
+                reject(err);
+            });
+        });
+    }
+
+static createActivity( name, description, date, hour, ubication, creator) {
+    return new Promise((resolve, reject) => {
+        let activityData={}
+            activityData={ 
+                name: name,
+                description: description,
+                date: date,
+                hour: hour,
+                ubication: ubication,
+                creator:creator          
+        }
+               
+        firebase.database().ref(activitiesPath + activityId).push(activityData).then(() => {
+            resolve();
+        }, err => {
+            reject(err);
+        });
+    });
+}
+
+static buildArray(firebaseData) {
+    let arrayData = [];
+    firebaseData.forEach(element => {
+        let item = element.val();
+        item.id = element.key;
+        arrayData.push(item);
+    });
+    return arrayData;
+}
+    
+}
